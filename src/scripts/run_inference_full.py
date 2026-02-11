83e8413936f7a2f3f10d19299b8649785129791e
ChathurangiShyalika <chathurangijks@gmail.com>
2026-02-10 23:29:06 -0500

    Update inference logic for hydraulic episodic store

--- src/scripts/run_inference_full.py
+++ src/scripts/run_inference_full.py
@@ -37,27 +37,27 @@ QA_PATH = "data/outputs/pdm/pdm_qa_action.jsonl"
 OUT_PATH = "data/outputs/pdm/preds_pdm_qa_action.jsonl"
 
 #6. python -m src.scripts.run_inference_full --start 0 --end 2205
-DB_PATH = "data/outputs/hydraulic/hyd_risk_model.joblib"
+DB_PATH = "data/outputs/hydraulic/episodic_store_hyd.db"
 QA_PATH = "data/outputs/hydraulic/hyd_diag_descriptive.jsonl"
 OUT_PATH = "data/outputs/hydraulic/preds_hyd_diag_descriptive.jsonl"
 
 #7. python -m src.scripts.run_inference_full --start 0 --end 2205
-DB_PATH = "data/outputs/hydraulic/hyd_risk_model.joblib"
+DB_PATH = "data/outputs/hydraulic/episodic_store_hyd.db"
 QA_PATH = "data/outputs/hydraulic/hyd_diag_diagnostic.jsonl"
 OUT_PATH = "data/outputs/hydraulic/preds_hyd_diag_diagnostic.jsonl"
 
 #8. python -m src.scripts.run_inference_full --start 0 --end 10
-DB_PATH = "data/outputs/hydraulic/hyd_risk_model.joblib"
+DB_PATH = "data/outputs/hydraulic/episodic_store_hyd.db"
 QA_PATH = "data/outputs/hydraulic/hyd_qas_temporal_count.jsonl"
 OUT_PATH = "data/outputs/hydraulic/preds_hyd_qas_temporal_count.jsonl"
 
 #9. python -m src.scripts.run_inference_full --start 0 --end 2184
-DB_PATH = "data/outputs/hydraulic/hyd_risk_model.joblib"
+DB_PATH = "data/outputs/hydraulic/episodic_store_hyd.db"
 QA_PATH = "data/outputs/hydraulic/hyd_cf.jsonl"
 OUT_PATH = "data/outputs/hydraulic/preds_hyd_cf.jsonl"
 
 #10. python -m src.scripts.run_inference_full --start 0 --end 2205
-DB_PATH = "data/outputs/hydraulic/hyd_risk_model.joblib"
+DB_PATH = "data/outputs/hydraulic/episodic_store_hyd.db"
 QA_PATH = "data/outputs/hydraulic/hyd_action.jsonl"
 OUT_PATH = "data/outputs/hydraulic/preds_hyd_action.jsonl"
 
